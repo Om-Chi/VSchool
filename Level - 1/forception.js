805f8628d7ab7604e117a834abe84dddcbd85c47
Oma Awa <omachi.awa@gmail.com>
2021-02-16 22:55:09 -0600

    started forception and array methods practice

---
+++ Level - 1/forception.js
@@ -0,0 +1,51 @@
+// add ":" somehow - line 4????
+// var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+// var alphabet = "abcdefghijklmnopqrstuvwxyz"
+//convert alphabet to uppercase
+// var upAlpha = alphabet.toUpperCase()
+//split() alphabet
+// var splitAlpha = upAlpha.split("")
+
+// function forception(ppl, alpha) {}
+// for (i = 0; i < people.length; i++){
+//     var eachPpl = [people[i]]
+//     console.log(eachPpl)
+//     for(j = upAlpha; j < eachPpl.length; j++) {
+//         var newArr = eachPpl.concat(upAlpha)
+//         console.log(newArr)
+//     }
+// }
+//END of Forception
+
+//"HELLOhello"
+// function strr(string1) {
+//    var string = string1
+//    var newUp = string.toUpperCase()
+//    var newLow = string.toLowerCase()
+//    return newUp + newLow
+// }
+// console.log(strr("hello"))
+
+//find the length, divide by 2, math.floor() it
+// function numLength(string2) {
+//     var string = string2
+//     var lengTH = Math.floor((string2.length) / 2)
+//     return lengTH
+// }
+// console.log(numLength("SugarHoneyIceTea"))
+
+// function sliced(string3) {
+//     var string = string3
+//     var halfLength = Math.floor(string.length / 2)
+//     return string.slice(0, halfLength)
+// }
+// console.log(sliced("Strawberry"))
+
+function upLowr(string4) {
+    var string = string4
+    var halfLength = Math.floor(string.length / 2)
+    var firstHalf = string.slice(0, halfLength)
+    var secondHalf = string.slice(halfLength)
+    return firstHalf.toUpperCase() + secondHalf.toLowerCase()
+}
+console.log(upLowr("hello"))
