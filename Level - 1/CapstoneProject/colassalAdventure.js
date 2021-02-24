b1f7a28be119ff5e601e6ffdbb28683ccf66f0d9
Oma Awa <omachi.awa@gmail.com>
2021-02-24 11:54:16 -0600

    Capstone - framework

---
+++ Level - 1/CapstoneProject/colassalAdventure.js
@@ -0,0 +1,12 @@
+//Greet Player w/ message  - SINGLE KEY readline sync
+//Ask and store name in variable, const - QUESTION readline sync
+
+//function: "w" equals random 1/4 chance of being approached to be attacked - SINGLE KEY MATH.CEIL() MATH.FLOOR() MATH.RANDOM() (max4 - min3) + min3
+    //attack or run
+    //if run, random amount of damage, 50% of escaping, user receives message and must "w" - QUESTION readline sync
+    //if attack: Warn user, a random enemy out of 3 is selected, random amount of damage - SINGLE KEY, MATH.RANDOM(), QUESTION readline sync 
+        //show current HP after killing enemy, continue walking - WHILE LOOP
+    //if enemy wins, cool message, game over - SINGLE KEY
+//INVENTORY - array
+    //player wins, player is awarded items - push() items inplayer inventory
+    //player hits 'p' or 'print', console prints players name, HP and inventory - constructor function
