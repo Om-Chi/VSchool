d522d2011b10b49657e5149e41fdb89da581c836
Oma Awa <omachi.awa@gmail.com>
2021-03-04 23:03:34 -0600

    RPG almost complete, issue with while loop

--- Level - 1/practice.js
+++ Level - 1/practice.js
@@ -1,24 +1,24 @@
-const readlineSync = require('readline-sync');
+// const readlineSync = require('readline-sync');
 
-//Welcome message
-//Rules
-readlineSync.keyInPause("Welcome to the colossus of all adventures...Colossal Adventure")
-readlineSync.keyInPause("Travel far lands and defeat the worst villians that pillage and devastate the land.")
-readlineSync.keyInPause("Rules: Along the way you may win awards and gather items to help you on your way.\nWhen approached by an enemy you may cower and run\nbut you will lose HP.")
-readlineSync.keyInPause("However, if you stand your ground and defeat said foe\nyou will gain HP and more rewards!")
-//User input name
-let playerOne = readlineSync.question('What is your name? ')
-console.log(`Greetings, ${playerOne} and safe travels...`)
+// //Welcome message
+// //Rules
+// readlineSync.keyInPause("Welcome to the colossus of all adventures...Colossal Adventure")
+// readlineSync.keyInPause("Travel far lands and defeat the worst villians that pillage and devastate the land.")
+// readlineSync.keyInPause("Rules: Along the way you may win awards and gather items to help you on your way.\nWhen approached by an enemy you may cower and run\nbut you will lose HP.")
+// readlineSync.keyInPause("However, if you stand your ground and defeat said foe\nyou will gain HP and more rewards!")
+// //User input name
+// let playerOne = readlineSync.question('What is your name? ')
+// console.log(`Greetings, ${playerOne} and safe travels...`)
 
 //player - array/object to store awards, and empty array to hold user's awards
-let awardsToWin = ['Dagger', 'Book of Secrets', 'Shield', 'First-Aid', 'Snacks', 'Booster-Boots']
-let consolationPrize = ['snacks', 'beets&Greens', 'water', 'sugar water', 'more snacks', 'ice', 'goat milk']
-let player = {Player1: playerOne, HP: 25}
-let awards = []
+// let awardsToWin = ['Dagger', 'Book of Secrets', 'Shield', 'First-Aid', 'Snacks', 'Booster-Boots']
+// let consolationPrize = ['snacks', 'beets&Greens', 'water', 'sugar water', 'more snacks', 'ice', 'goat milk']
+// let player = {Player1: playerOne, HP: 25}
+// let awards = []
 
 //array of Enemies
 let galleryOfFoes = [{enemy: "Babla the Funk", attackPTS: 10}, {enemy: "Scion the Grudge", attackPTS: 20}, {enemy: "Tempa the Misfit", attackPTS: 30}]
-
+let array = []
 //FUNCTIONS
 const printPlayerInfo = (selection) => {
     //print player info & inventory
@@ -32,45 +32,20 @@ function randomItem(min, max) {
     return mathF
 }
 
-const theFight = () => {
-    let winLose = Math.floor(Math.random() * 2) + 1
-    if (winLose === 1) {
-        console.log('Congratulaions! You have defeated the opponent!')
-    }else {
-        console.log("Shame! Continue on your journey and grow stronger!")
-    }return winLose
-    
-}
-const toRun = (selection) => {
-    //return option to "walk" or "print inventory"
-}
-const toWalk = (selection) => {
-    //random message to find something along the way
-    //return option to "walk" or "print inventory"
-}
-//Random Random - for HP and chances to escape
-const extraRandom = (max) => {
-    let final = Math.floor(Math.random() * max) + 1
-    return final
-}
+// const theFight = (max) => {
+//     let winLose = Math.floor(Math.random() * max) + 1
+//     if (winLose % 2 === 0) {
+//         console.log('Congratulaions! You have defeated the opponent!')
+//     }else {
+//         console.log("Shame! Continue on your journey and grow stronger!")
+//     }
+// }
+// theFight(40)
 
-//USER INPUT
-var userSelection = readlineSync.question('Would you like to take a walk? TYPE "w" or TYPE "p" to check player ', {limit:'wp'})
-if (userSelection === 'p') {
-    console.log(printPlayerInfo("p"))
+const prizes = () => {
+    var popped = galleryOfFoes.pop()
+    array.push(popped)
+    console.log(array)
 }
-if (userSelection === 'w') {
-    console.log("As you continue on your journey...")
-}
+prizes()
 
-
-player.hp = 25
-//while (player.HP > 0) {}
-var randomWalk = extraRandom(4)
-if (randomWalk === 2) {
-    toFight()
-    player.HP -= foe.attackPTS
-    console.log(player)
-    console.log(awards)
-}
-var fightFlight = readlineSync.question(`Look out!!! ${galleryOfFoes[index]} approaches! What will you do? Type "run" or "fight" `)
